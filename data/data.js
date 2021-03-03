84298027633d0d8a9fd21ce3307ee33f5ca07db0
JennyViannay <jenny.viannay75@gmail.com>
2021-03-03 21:47:35 +0100

    init map students remote 0321

---
+++ data/data.js
@@ -0,0 +1,344 @@
+var data = [
+    {
+        "name": "Julien Fourneau",
+        "lat": 48.310876429847745,
+        "lng": 1.9968119466626884,
+        "city" : "Angerville",
+        "language" : "",
+        "image" : "assets/images/julienf.jpg"
+    },
+    {
+        "name": "Kévin Touyard",
+        "lat": 43.93379969051903,
+        "lng": 4.612094994132545,
+        "city" : "Fourmès",
+        "language" : "",
+        "image" : "assets/images/kevint.jpg"
+    },
+    {
+        "name": "Milad Behzadi",
+        "lat": 48.539749191101784,
+        "lng": 2.6602934410904586,
+        "city" : "77",
+        "language" : "",
+        "image" : "assets/images/milad.jpg"
+    },
+    {
+        "name": "Sophie Topart",
+        "lat": 48.55922391149755,
+        "lng": 2.2777083712894606,
+        "city" : "Cheptainville",
+        "language" : "",
+        "image" : "assets/images/sophie.jpg"
+    },
+    {
+        "name": "Colin Le Gac",
+        "lat": 48.11717087644704,
+        "lng": -1.6812451198241174,
+        "city" : "Rennes",
+        "language" : "",
+        "image" : "assets/images/colin.jpg"
+    },
+    {
+        "name": "Manon Josse",
+        "lat": 48.77468202679652,
+        "lng": 2.5534115307797425,
+        "city" : "",
+        "language" : "Noiseau",
+        "image" : "assets/images/manon.jpeg"
+    },
+    {
+        "name": "Julien Bourgineau",
+        "lat": 35.97960605087289,
+        "lng": 14.348129900417685,
+        "city" : "Malte",
+        "language" : "",
+        "image" : "assets/images/julienb.jpg"
+    },
+    {
+        "name": "Hugo Guillaume",
+        "lat": 43.29380044574117,
+        "lng": 5.394909133977265,
+        "city" : "Marseille",
+        "language" : "",
+        "image" : "assets/images/hugo.jpg"
+    },
+    {
+        "name": "Baptiste Grias",
+        "lat": 47.234237818660375,
+        "lng": -1.5604010294183819,
+        "city" : "Nantes",
+        "language" : "",
+        "image" : "assets/images/baptiste.jpg"
+    },
+    {
+        "name": "Pauline Guiho Blanchard",
+        "lat": 47.234237818660375,
+        "lng": -1.5604010294183819,
+        "city" : "Nantes",
+        "language" : "",
+        "image" : "assets/images/paulineg.jpg"
+    },
+    {
+        "name": "Eddy Rajaonarivelo",
+        "lat": 47.752218967802946,
+        "lng": 7.328714004538136,
+        "city" : "Mulhouse",
+        "language" : "",
+        "image" : "assets/images/eddy.jpg"
+    },
+    {
+        "name": "Jody Gauthier",
+        "lat": 49.03957827606279,
+        "lng": 3.940549728458742,
+        "city" : "Epernay",
+        "language" : "",
+        "image" : "assets/images/jody.jpg"
+    },
+    {
+        "name": "Amine Haddad",
+        "lat": 48.00051933269934,
+        "lng": 2.734917738331538,
+        "city" : "Montargis",
+        "language" : "",
+        "image" : "assets/images/amine.jpg"
+    },
+    {
+        "name": "Gregory Bouteville",
+        "lat": 43.40231231863299,
+        "lng": -1.4767434033343292,
+        "city" : "Ustaritz",
+        "language" : "",
+        "image" : "assets/images/gregory.jpg"
+    },
+    {
+        "name": "Yannis Blondron",
+        "lat": 48.69225313495438,
+        "lng": 2.376293797794784,
+        "city" : "Juvisy-Sur-Orge",
+        "language" : "",
+        "image" : "assets/images/yannis.jpg"
+    },
+    {
+        "name": "Justine Giletta",
+        "lat": 48.88548254142721,
+        "lng": 2.4055971050049973,
+        "city" : "Pré-St-Gervais",
+        "language" : "",
+        "image" : "assets/images/justine.jpg"
+    },
+    {
+        "name": "Anaïs BERDUCAT",
+        "lat": 47.22222114473467,
+        "lng": 1.3571053047970143,
+        "city" : "Chateauvieux",
+        "language" : "",
+        "image" : "assets/images/anais.jpg"
+    },
+    {
+        "name": "Wissam Taleb",
+        "lat": 48.79152774451419,
+        "lng": 1.8884359111346325,
+        "city" : "78",
+        "language" : "",
+        "image" : "assets/images/wissam.jpg"
+    },
+    {
+        "name": "Edouard Larroche",
+        "lat": 48.86403265662592,
+        "lng": 2.3474016675050007,
+        "city" : "Paris",
+        "language" : "",
+        "image" : "assets/images/edouard.jpg"
+    },
+    {
+        "name": "Aline Dubois",
+        "lat": 44.84667004098226,
+        "lng": -0.5871726479462603,
+        "city" : "Bordeaux",
+        "language" : "",
+        "image" : "assets/images/aline.jpg"
+    },
+    {
+        "name": "Alexandre Morlat",
+        "lat": 47.98902127981969,
+        "lng": 2.7697887902288434,
+        "city" : "Amilly",
+        "language" : "",
+        "image" : "assets/images/alexandre.jpg"
+    },
+    {
+        "name": "Violaine Ernotte",
+        "lat": 48.86990452233246,
+        "lng": 2.3813906181117104,
+        "city" : "Paris",
+        "language" : "",
+        "image" : "assets/images/violaine.jpg"
+    },
+    {
+        "name": "Julien Nicolas Brachet-Randoulet",
+        "lat": 43.523008997312076,
+        "lng": 5.432920558728173,
+        "city" : "Aix-en-provence",
+        "language" : "",
+        "image" : "assets/images/jn.jpg"
+    },
+    {
+        "name": "Tom Carpentier",
+        "lat": 43.603413349471126,
+        "lng": 1.4331826052952095,
+        "city" : "Toulouse",
+        "language" : "",
+        "image" : "assets/images/tom.jpg"
+    },
+    {
+        "name": "Guillaume Joulia",
+        "lat": 43.300297145555746,
+        "lng": 5.41550849798133,
+        "city" : "Marseille",
+        "language" : "",
+        "image" : "assets/images/guillaume.jpg"
+    },
+    {
+        "name": "Antoine Duriez",
+        "lat": 47.22771013832012,
+        "lng": -1.538771697214112,
+        "city" : "Nantes",
+        "language" : "",
+        "image" : "assets/images/antoined.jpg"
+    },
+    {
+        "name": "Sylvain Laconi",
+        "lat": 48.8477400710608,
+        "lng": 2.2081700310487773,
+        "city" : "Saint Cloud",
+        "language" : "",
+        "image" : "assets/images/sylvain.jpg"
+    },
+    {
+        "name": "Fanny Lemaitre-Hermenier",
+        "lat": 47.18596061079308,
+        "lng": -1.5634909340189918,
+        "city" : "Nantes",
+        "language" : "Javascript",
+        "image" : "assets/images/fanny.jpg"
+    },
+    {
+        "name": "Marie-Anne Duvieu",
+        "lat": 48.81414077361756,
+        "lng": 1.799172000450345,
+        "city" : "78",
+        "language" : "",
+        "image" : "assets/images/maire-anne.jpg"
+    },
+    {
+        "name": "Sarah Bensouna",
+        "lat": 43.59769029749263,
+        "lng": -1.439331475557467,
+        "city" : "Labenne",
+        "language" : "",
+        "image" : "assets/images/sarah.jpg"
+    },
+    {
+        "name": "Anne Gren",
+        "lat": 43.30429476966679,
+        "lng": 5.445034253053826,
+        "city" : "Marseille",
+        "language" : "",
+        "image" : "assets/images/anne.jpg"
+    },
+    {
+        "name": "Olivier Maffre",
+        "lat": 43.96927168847028,
+        "lng": 0.37918981155859677,
+        "city" : "Condom",
+        "language" : "",
+        "image" : "assets/images/olivier.jpg"
+    },
+    {
+        "name": "Pierre Houot",
+        "lat": 48.78996643698495,
+        "lng": 2.4481036478134386,
+        "city" : "Créteil",
+        "language" : "",
+        "image" : "assets/images/pierre.jpg"
+    },
+    {
+        "name": "Mickael Garatens",
+        "lat": 43.39662626973258,
+        "lng": -1.636097030595427,
+        "city" : "Saint Jean De Luz",
+        "language" : "",
+        "image" : "assets/images/mickag.jpg"
+    },
+    {
+        "name": "Pauline Labbé",
+        "lat": 48.868549537555495,
+        "lng": 2.351178217572413,
+        "city" : "Paris",
+        "language" : "",
+        "image" : "assets/images/pualinel.jpg"
+    },
+    {
+        "name": "Jérôme Shaaban",
+        "lat": 43.602170283790784,
+        "lng": 1.4149865004249509,
+        "city" : "Toulouse",
+        "language" : "",
+        "image" : "assets/images/jerome.jpg"
+    },
+    {
+        "name": "Franck Bouchet",
+        "lat": 43.607596827797316,
+        "lng": 1.433525850164616,
+        "city" : "Lissac",
+        "language" : "",
+        "image" : "assets/images/franck.jpg"
+    },
+    {
+        "name": "Kevin Janson",
+        "lat": 45.60265424982191,
+        "lng": 5.276435647142822,
+        "city" : "Bourgoin-Jaillieu",
+        "language" : "",
+        "image" : "assets/images/kevinj.jpg"
+    },
+    {
+        "name": "David D'Amato",
+        "lat": 48.57749735785289,
+        "lng": 7.749389494343817,
+        "city" : "Strasbourg",
+        "language" : "",
+        "image" : "assets/images/david.jpg"
+    },
+    {
+        "name": "Tony Alastrue",
+        "lat": 48.601732529752866,
+        "lng": 2.4641777838253653,
+        "city" : "Corbeil-Essonne",
+        "language" : "",
+        "image" : "assets/images/tony.jpg"
+    },
+    {
+        "name": "Jenny",
+        "lat": 0,
+        "lng": 0,
+        "city" : "Paris",
+        "language" : "PHP",
+        "image" : ""
+    },
+    {
+        "name": "Katalina",
+        "lat": 0,
+        "lng": 0,
+        "city" : "",
+        "language" : "JS",
+        "image" : ""
+    },
+]
+
+var jsName = document.getElementById('js-name')
+var city = document.getElementById('city')
+var language = document.getElementById('language')
+var avatar = document.getElementById('avatar')
+  
